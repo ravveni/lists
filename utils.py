6bc9903b176c6503b19f1f5b80effc14b452d8e0
ravveni <ravveni420@gmail.com>
2023-05-25 10:46:23 -0400

    added util functions, separated canada cities from provinces

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@
 def find_longest_string(filepath):
     file = open(filepath, 'r')
     lines = file.readlines()
+    file.close()
 
     longest_string = ""
 
@@ -14,8 +15,38 @@ def find_longest_string(filepath):
 
     print("Longest string in " + filepath + ": " + longest_string)
 
-find_longest_string('people/first_names.txt')
-find_longest_string('people/last_names.txt')
+def remove_duplicates_from_list(filepath):
+    file = open(filepath, 'r')
+    lines = file.readlines()
+    file.close()
 
-find_longest_string('people/femme_names.txt')
-find_longest_string('people/masc_names.txt')
+    file = open(filepath, 'w')
+    no_dup_lines = sorted(list(dict.fromkeys(lines)))
+
+    for no_dup_line in no_dup_lines:
+        file.write(no_dup_line) # adjust new line as needed
+
+    file.close()
+
+def separate_commaed_list(filepath):
+    split_filepath = filepath.split("/")
+    folder = split_filepath[0]
+    filename = split_filepath[1].split(".")[0]
+    new_base_filepath = folder + "/" + filename
+
+    first_column = open(new_base_filepath + "_0.txt", 'w')
+    second_column = open(new_base_filepath + "_1.txt", 'w')
+
+    file = open(filepath, 'r')
+    lines = file.readlines()
+    file.close()
+
+    for line in lines:
+        split_line = line.split(", ")
+        first_column.write(split_line[0] + "\n") # adjust new line as needed
+        second_column.write(split_line[1])
+
+    first_column.close()
+    second_column.close()
+
+remove_duplicates_from_list("geography/canadian_us_cities.txt")
